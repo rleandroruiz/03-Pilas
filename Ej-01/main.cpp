f3d64d275c73046b0fa8ca20da0b0c785d723593
rleandroruiz <leo1998.04.24@hotmail.com>
2017-09-06 11:53:55 -0300

    Avances en ej 1 y ej 2

--- Ej-01/main.cpp
+++ Ej-01/main.cpp
@@ -1,7 +1,15 @@
 #include <iostream>
+#include <string>
 #include "../Pila/Pila.h"
+
 
 int main() {
     std::cout << "Ejercicio 03/01\n" << std::endl;
+    char *palabra;
+
+    std::cout<<"Ingrese una palabra"<<std::endl;
+    std::getline(std::cin, palabra);
+    std::cout<<palabra<<std::endl;
+
     return 0;
 }
--- Ej-02/main.cpp
+++ Ej-02/main.cpp
@@ -3,5 +3,27 @@
 
 int main() {
     std::cout << "Ejercicio 03/02\n" << std::endl;
+
+    Pila<int> a, b;
+
+    a.push(1);
+    b.push(3);
+
+
+    int s = 1;
+
+    while((!a.esVacia() || !b.esVacia()) && s == 1) {
+
+        if (a.pop() != b.pop())
+            s = 0;
+
+    }
+
+    if(s = 1)
+        std::cout << "Las pilas son iguales" << std::endl;
+
+    else
+        std::cout << "Las pilas NO son iguales" << std::endl;
+
     return 0;
 }
